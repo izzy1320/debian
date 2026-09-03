28b59d5df4011bc4e352b13d234f545421e71090
izzy1320 <izineth@proton.me>
2026-09-03 07:41:42 -0400

    Create nvidia.sh

---
+++ nvidia.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# Exit if error
+set -e 
+
+# Download the keychain & repository
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/cuda-keyring_1.1-1_all.deb
+
+# Install the keychain
+sudo dpkg -i ./cuda-keyring_1.1-1_all.deb
+
+# Update
+sudo apt update
+
+# Pin the 610 branch
+sudo apt install -y nvidia-driver-pinning-610
+
+# Install Driver
+sudo apt install -y linux-headers-$(uname -r)
+sudo apt install -V -y nvidia-open
+
+# Back up the current GRUB configuration just in case
+sudo cp /etc/default/grub /etc/default/grub.bak
+
+# Inject parameters inside the existing quotes of GRUB_CMDLINE_LINUX_DEFAULT
+sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/ s/"$/ intel_pstate=active nvidia-drm.modeset=1"/' /etc/default/grub
+
+# Update GRUB configurations to apply changes
+sudo update-grub
